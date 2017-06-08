0ffb16fb86220eec3f909f1b22628a4eaa7f7346
Ellen <chenellen007@gmail.com>
2017-06-08 14:09:04 +0800

    Add files via upload

---
+++ src/main/java/com/example/tinnytimer/AddTimerActivity.java
@@ -0,0 +1,13 @@
+package com.example.tinnytimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_timer);
+    }
+}
